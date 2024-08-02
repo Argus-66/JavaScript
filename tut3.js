4abdb5a4b3e98075f01967731eb7ca941f5a7dbc
Ayush H <jqghebri@gmail.com>
2024-08-02 22:11:57 +0530

    creation

--- tut3.js
+++ tut3.js
@@ -3,4 +3,8 @@ const dc_heroes = ["superman", "flash", "batman"]
 
 marvel_heroes.push(dc_heroes)
 
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+marvel_heroes.concat(dc_heroes);
 console.log(marvel_heroes);
