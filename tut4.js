5b9c4b833e77728029c9daeee8162f776a0083df
Ayush H <jqghebri@gmail.com>
2024-08-03 00:46:50 +0530

    creation

--- tut4.js
+++ tut4.js
@@ -1,8 +1,11 @@
 //Object literals
+const mySym = Symbol("key1")
+console.log(mySym);
 
 const JsUser = {
     name: "Argus",
     age: 20,
+    mySym: "mykey1",
     location: "New York",
     email: "argus@example.com",
     isLoggedIn: true,
@@ -12,4 +15,27 @@ const JsUser = {
 console.log((JsUser));
 console.log(JsUser.name);
 console.log(JsUser.lastLoginDate[1]);
+console.log(JsUser.email);
 console.log(JsUser["email"]);
+console.log(Object.keys(JsUser));
+
+console.log("-----------------------");
+console.log(JsUser.mySym);
+console.log(JsUser[mySym]);
+console.log(typeof mySym);
+console.log(typeof JsUser.mySym);
+
+console.log("-----------------------");
+const mySym2 = Symbol("key1")
+
+const JsUser2 = {
+    [mySym2]: "mykey1",
+};
+
+console.log(JsUser2[mySym2]);
+console.log(typeof mySym2);
+console.log(typeof [mySym2]);
+console.log(typeof JsUser2[mySym2]);
+
+//Value overwriting
+JsUser.email = "argus@gmail.com";
