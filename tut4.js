b2679b2ad4a8baacc641d0dc39af7bc47e2f523f
Ayush H <jqghebri@gmail.com>
2024-08-03 00:32:14 +0530

    creation

---
+++ tut4.js
@@ -0,0 +1,14 @@
+//Object literals
+
+const JsUser = {
+    name: "Argus",
+    age: 20,
+    location: "New York",
+    email: "argus@example.com",
+    isLoggedIn: true,
+    lastLoginDate: ["Monday", "Saturday"]
+};
+
+console.log((JsUser));
+console.log(JsUser.name);
+console.log(JsUser.lastLoginDate[1]);
