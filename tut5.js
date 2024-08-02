45203797142b6fb5a5d4f1617405a77915685fb8
Ayush H <jqghebri@gmail.com>
2024-08-03 01:11:03 +0530

    creation

--- tut5.js
+++ tut5.js
@@ -1,5 +1,22 @@
 // Object Singletons/Constructors
 
 const tinderUser = new Object();
+// const tinderUser = {}; can use this as well
+
+tinderUser.id = "123abc"
+tinderUser.name = "Argus";
+tinderUser.isLoggedIn = true;
 
 console.log(tinderUser);
+
+const regularUser = {
+    email: "argus@example.com",
+    fullname: {
+        userfullname: {
+            firstname: "Argus",
+            lastname: "-66"
+        }
+    }
+}
+console.log(regularUser);
+console.log(regularUser.fullname);
