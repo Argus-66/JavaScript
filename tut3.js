b0be345f1d68b30bb2b9844c7c641467cdfce03b
Ayush H <jqghebri@gmail.com>
2024-08-02 22:22:23 +0530

    creation

--- tut3.js
+++ tut3.js
@@ -16,3 +16,14 @@ console.log(another_array);
 
 const real_another_array = another_array.flat(Infinity);
 console.log(real_another_array);
+
+console.log(Array.isArray(real_another_array));
+console.log(Array.isArray("Argus"));
+console.log(Array.from("Argus"));
+console.log(Array.from({name: "Argus"}));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3)); 
