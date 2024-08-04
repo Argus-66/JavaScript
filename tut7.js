28f141c3e7d889caedf3c626a0f0e36eea7d6de9
Ayush H <jqghebri@gmail.com>
2024-08-04 17:59:10 +0530

    creation

--- tut7.js
+++ tut7.js
@@ -1,13 +1,44 @@
-function one(){
-    const username = "John Doe";
+function one() {
+  const username = "John Doe";
 
-    function two(){
-        const website = "https://www.example.com";
-        console.log(`Username: ${username}, Website: ${website}`);
-    }
-    //console.log(website); // ReferenceError: website is not defined
+  function two() {
+    const website = "https://www.example.com";
+    console.log(`Username: ${username}, Website: ${website}`);
+  }
+  //console.log(website); // ReferenceError: website is not defined
 
-    two();
+  two();
 }
 
 one();
+
+console.log("-------------------------------");
+
+if (true) {
+  const username = "Jane";
+  if (username === "Jane") {
+    const website = " https://www.jane.com";
+    console.log(username + website);
+    console.log(`Username: ${username}, Website: ${website}`);
+  }
+  //console.log(website); // ReferenceError: website is not defined
+}
+
+//console.log(username); // John Doe
+
+console.log("-------------------------------");
+
+
+
+
+function addone(num){
+    return num + 1;
+}
+
+console.log(addone(5));
+
+const addtwo = function(num){
+    return num + 2;
+}
+
+console.log(addtwo(5));
