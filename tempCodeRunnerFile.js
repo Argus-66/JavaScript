5b6dcbbc86848b48a251af9a4029133ab0ec63a7
Ayush H <jqghebri@gmail.com>
2024-08-04 16:19:27 +0530

    creation

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-Object.freeze(JsUser);
-// JsUser.email = "argus@yahoo.com";
-// console.log(`${JsUser.email} ---------Value frozen`);
+console.log("-------------------------------");
--- tut6.js
+++ tut6.js
@@ -10,8 +10,32 @@ function sayMyName(){
 
 sayMyName();
 
+console.log("-------------------------------");
+
 function addTwoNumbers( number1, number2){
+
     console.log(number1+number2)
 }
 
-addTwoNumbers(3,4);
+const result = addTwoNumbers(3,4);
+console.log(result);
+
+console.log("-------------------------------");
+
+function addTwoNumbers2( number1, number2){
+    //let result = number1 + number2;
+    //return result;
+    return number1 + number2;
+}
+
+const result2 = addTwoNumbers2(5,7);
+console.log("Result: ", result2);
+
+console.log("-------------------------------");
+
+function loginUserMessage(username){
+    return `${ username }! just logged in.`;
+}
+
+console.log(loginUserMessage("John Doe"))
+console.log(loginUserMessage())
