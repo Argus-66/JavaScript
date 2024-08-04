fdee37ca19681820f2c4eb0c063b11391e5a15f2
Ayush H <jqghebri@gmail.com>
2024-08-04 16:10:25 +0530

    creation

---
+++ tut6.js
@@ -0,0 +1,17 @@
+// Functions---------------------------------------
+
+function sayMyName(){
+    console.log("A");
+    console.log("R");
+    console.log("G");
+    console.log("U");
+    console.log("S");
+}
+
+sayMyName();
+
+function addTwoNumbers( number1, number2){
+    console.log(number1+number2)
+}
+
+addTwoNumbers(3,4);
