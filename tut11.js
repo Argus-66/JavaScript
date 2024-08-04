1f7c7ef8d36818361a4707f5285ddbd01d1f4dfb
Ayush H <jqghebri@gmail.com>
2024-08-04 23:04:24 +0530

    creation

---
+++ tut11.js
@@ -0,0 +1,17 @@
+// Iterations 
+
+    // For loop
+    // While loop
+    // Do-while loop
+    // forEach loop
+    // map
+
+// For loop
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+console.log("------------------------------")
+
+
