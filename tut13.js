22ae4231d2b91275fe818f33029248592c898518
Ayush H <jqghebri@gmail.com>
2024-08-06 01:01:22 +0530

    creation

--- tut13.js
+++ tut13.js
@@ -1,6 +1,16 @@
 const coding = [ "js", "python", "java", "c", "cpp", "go" ];
 
 
-coding.forEach((item) => {
+const values = coding.forEach((item) => {
     console.log(item);
+    return item
 })
+
+console.log(values);
+
+console.log("-----------------------------");
+
+const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+const newNums = myNums.filter((num) => num>4 )
+console.log(newNums);
