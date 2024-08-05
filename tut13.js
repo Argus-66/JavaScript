9c72597d0ea7c3c7115da3a4a642b170287c9396
Ayush H <jqghebri@gmail.com>
2024-08-06 01:05:25 +0530

    creation

--- tut13.js
+++ tut13.js
@@ -14,3 +14,23 @@ const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
 const newNums = myNums.filter((num) => num>4 )
 console.log(newNums);
+
+console.log("-----------------------------");
+
+const newNums2 = myNums.filter((num) => {
+    return num>4;
+})
+
+console.log(newNums2);
+
+console.log("-----------------------------");
+
+const newNums3 = []
+
+myNums.forEach((num) => {
+    if(num > 4) {
+        newNums3.push(num);
+    }
+})
+
+console.log(newNums3);
