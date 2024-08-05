5b016df987fc36914e64f55bb83cb198efd8a33a
Ayush H <jqghebri@gmail.com>
2024-08-05 10:22:20 +0530

    creation

--- tut12.js
+++ tut12.js
@@ -26,3 +26,63 @@ console.log("-----------------------------");
 for (const key in programming) {
     console.log(programming[key]);
 }
+
+console.log("-----------------------------");
+
+//For Each Loop
+
+const coding = ["js", "python", "java", "c", "cpp", "go"];
+
+coding.forEach(function(item) {
+    console.log(item);
+});
+
+console.log("-----------------------------");
+
+coding.forEach((item) =>{
+    console.log(item)
+});
+
+console.log("-----------------------------");
+
+function printMe(item) {
+    console.log(item);
+}
+
+coding.forEach(printMe);
+
+console.log("-----------------------------");
+
+coding.forEach((item, index, arr) => {
+    console.log(item, index, arr);
+});
+
+console.log("-----------------------------");
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFileName: "js",
+    },
+    {
+        languageName: "Python",
+        languageFileName: "python",
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java",
+    },
+    {
+        languageName: "C",
+        languageFileName: "c",
+    },
+    {
+        languageName: "C++",
+        languageFileName: "cpp",
+    }
+]
+
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+});
