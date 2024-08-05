7352949b80b8b62092adfaf348a96a88966356d6
Ayush H <jqghebri@gmail.com>
2024-08-06 01:59:03 +0530

    creation

--- tut16.js
+++ tut16.js
@@ -1,6 +1,7 @@
 const myNums = [1, 2, 3, 4, 5 ]
 
 const  myTotal = myNums.reduce(function (accumulator, currentValue) {
+    console.log(`accumulator: ${accumulator} and currentValue: ${currentValue}`)
     return accumulator + currentValue
 
 }, 0)
