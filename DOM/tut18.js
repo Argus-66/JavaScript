68b04dfb1367414f0074c3f6d3497dac96b4dc5d
Ayush H <jqghebri@gmail.com>
2024-08-06 22:29:11 +0530

    creation

---
+++ DOM/tut18.js
@@ -0,0 +1,13 @@
+title.style.backgroundColor = 'green';
+title.style.color = 'lightblue';
+
+console.log(title.innerHTML);
+console.log(title.style.backgroundColor);
+console.log(title.innerText);
+console.log(title.textContent);
+
+console.log(document.querySelector('h2'));
+console.log(document.querySelector('#title'));
+console.log(document.querySelector('.heading'));
+console.log(document.querySelector('input[type = "password"]'));
+
