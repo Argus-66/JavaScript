9f6f083350ffa4da005207831d0df1050fbd4d93
Ayush H <jqghebri@gmail.com>
2024-08-06 23:36:22 +0530

    creation

--- DOM/tut.19.js
+++ DOM/tut.19.js
@@ -2,3 +2,9 @@ const parent = document.querySelector('.parent')
 console.log(parent);
 
 console.log(parent.children);
+
+console.log(parent.children[2].innerHTML);
+
+for (let i = 0; i < parent.children.length; i++) {
+console.log(parent.children[i].innerHTML);
+}
